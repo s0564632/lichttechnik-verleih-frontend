de82aa19d1f011b487786f4cfd5e05bab75e4da1
s0564632 <66623031+s0564632@users.noreply.github.com>
2026-07-05 11:29:49 +0200

    "test: remove invalid Equipment interface spec file"

--- src/app/services/equipment.ts
+++ src/app/services/equipment.ts
@@ -1,4 +1,17 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Equipment } from '../interfaces/equipment.interface';
 
-@Service()
-export class Equipment {}
+@Injectable({
+  providedIn: 'root'
+})
+export class EquipmentService {
+  private apiUrl = 'http://localhost:3000/api/equipment';
+
+  constructor(private http: HttpClient) {}
+
+  getEquipment(): Observable<Equipment[]> {
+    return this.http.get<Equipment[]>(this.apiUrl);
+  }
+}
