e1583ec50f99cba4681c256a6192c117fc3e702d
s0564632 <s0564632@htw-berlin.de>
2026-09-15 18:45:04 +0200

    feat(fe): Verwaltungsoberfläche als eigene Route auslagern

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Verwaltung } from './verwaltung/verwaltung';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'verwaltung', component: Verwaltung }
+];
