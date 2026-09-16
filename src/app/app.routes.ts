bc5290b9c7cff41a1d90e09fd72461eeebd247e0
s0564632 <s0564632@htw-berlin.de>
2026-09-16 13:10:45 +0200

    feat(fe): Landingpage für den öffentlichen Shop hinzufügen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Verwaltung } from './verwaltung/verwaltung';
 import { Shop } from './shop/shop';
+import { Startseite } from './startseite/startseite';
 
 export const routes: Routes = [
-  { path: 'verwaltung', component: Verwaltung },
-  { path: 'shop', component: Shop }
+    { path: 'shop', component: Shop },
+    { path: 'verwaltung', component: Verwaltung },
+    { path: 'startseite', component: Startseite },
 ];
---
+++ src/app/startseite/startseite.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-startseite',
+  imports: [RouterLink],
+  templateUrl: './startseite.html',
+  styleUrl: './startseite.css',
+})
+export class Startseite {}
