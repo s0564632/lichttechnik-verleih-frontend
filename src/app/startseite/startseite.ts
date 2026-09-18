fded9f58ff89f098245c285cb46474c56b74f1da
s0564632 <s0564632@htw-berlin.de>
2026-09-18 10:41:31 +0200

    Navbar und Startseite mit dynamischem Control Flow und CSS-Grid aktualisiert

--- src/app/startseite/startseite.ts
+++ src/app/startseite/startseite.ts
@@ -7,4 +7,36 @@ import { RouterLink } from '@angular/router';
   templateUrl: './startseite.html',
   styleUrl: './startseite.css',
 })
-export class Startseite {}
+export class Startseite {
+  kategorieGruppen = [
+    {
+      titel: 'Scheinwerfer',
+      unterpunkte: ['PAR-Scheinwerfer', 'LED-Scheinwerfer', 'Profilscheinwerfer', 'LED-Bars', 'Stufenlinsen', 'Halogen-Scheinwerfer', 'Schwarzlicht']
+    },
+    {
+      titel: 'Lichteffekte',
+      unterpunkte: ['Nebelmaschinen', 'Effektmaschinen', 'Dekorative Beleuchtung']
+    },
+    {
+      titel: 'Lichtsteuerung',
+      unterpunkte: ['Lichtpulte & Controller', 'DMX-Interfaces', 'Lasersteuerung', 'Steuerungs-PC']
+    },
+    {
+      titel: 'Laser',
+      unterpunkte: ['Showlaser']
+    },
+    {
+      titel: 'Dimmer & Strom',
+      unterpunkte: ['Dimmer', 'Netzteile', 'Stromverteilung']
+    },
+    {
+      titel: 'Kabel & Adapter',
+      unterpunkte: ['DMX-Signalverteilung', 'DMX-Kabel', 'Stromkabel', 'Verlängerungskabel', 'DMX-Adapter']
+    },
+    {
+      titel: 'Zubehör',
+      unterpunkte: ['Projektion', 'Event-Zubehör', 'LED-/Pixel-Zubehör']
+    }
+  ];
+}
+
