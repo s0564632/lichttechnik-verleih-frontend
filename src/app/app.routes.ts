1a1d17be712b40d495fea362dbff9c5394c3e97c
s0564632 <s0564632@htw-berlin.de>
2026-09-25 17:34:12 +0200

    refactor(home): ordnernamen der startseite auf kleingeschriebenes home-feature vereinheitlicht

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './HomeComponent/home.component';
-import { Verwaltung } from './verwaltung/verwaltung';
+import { HomeComponent } from './home/home.component';
 import { Shop } from './shop/shop';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'shop', component: Shop },
-  { path: 'verwaltung', component: Verwaltung },
   { path: '**', redirectTo: 'home' },
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { RouterLink,} from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, ],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
