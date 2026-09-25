42a3cb760ab8fddc1f0b8ac44706ae466ccbd852
s0564632 <s0564632@htw-berlin.de>
2026-09-25 22:48:44 +0200

    feat: Produktkategorien in Navigation ergänzt

--- src/app/core/components/navbar/navbar.ts
+++ src/app/core/components/navbar/navbar.ts
@@ -36,5 +36,23 @@ export class Navbar {
       titel: 'Laser',
       unterpunkte: ['Showlaser'],
     },
+    {
+      titel: 'Dimmer & Strom',
+      unterpunkte: ['Dimmer', 'Netzteile', 'Stromverteilung'],
+    },
+    {
+      titel: 'Kabel & Adapter',
+      unterpunkte: [
+        'DMX-Signalverteilung',
+        'DMX-Kabel',
+        'Stromkabel',
+        'Verlängerungskabel',
+        'DMX-Adapter',
+      ],
+    },
+    {
+      titel: 'Zubehör',
+      unterpunkte: ['Projektion', 'Event-Zubehör', 'LED-/Pixel-Zubehör'],
+    },
   ];
 }
